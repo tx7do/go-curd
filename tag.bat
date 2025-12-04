14c5b1bec8ac43ad30d9785f5aa98d282498e174
Bobo <yanglinbo@gmail.com>
2025-12-04 15:16:36 +0800

    feat: gorm mixin

--- tag.bat
+++ tag.bat
@@ -1,5 +1,5 @@
-git tag entgo/v0.0.10 --force
-git tag gorm/v0.0.3 --force
+git tag entgo/v0.0.11 --force
+git tag gorm/v0.0.4 --force
 
 git tag cassandra/v0.0.1 --force
 git tag clickhouse/v0.0.1 --force
