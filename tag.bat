9d6ff1eba125049c4e5924441c21a7f86ff3ee03
Bobo <yanglinbo@gmail.com>
2025-12-09 13:31:24 +0800

    feat: MongoDB client

--- tag.bat
+++ tag.bat
@@ -2,10 +2,10 @@ git tag entgo/v0.0.13 --force
 git tag gorm/v0.0.5 --force
 
 git tag cassandra/v0.0.1 --force
-git tag clickhouse/v0.0.1 --force
+git tag clickhouse/v0.0.2 --force
 git tag elasticsearch/v0.0.1 --force
-git tag influxdb/v0.0.1 --force
-git tag mongodb/v0.0.1 --force
+git tag influxdb/v0.0.2 --force
+git tag mongodb/v0.0.2 --force
 
 git tag v0.0.5 --force
 
